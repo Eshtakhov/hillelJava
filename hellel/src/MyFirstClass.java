679e8514ce5e1b74defa49ccb789095b5d0651aa
ES <shah1991@ukr.net>
2015-09-17 21:19:41 +0300

    hello world commit

---
+++ hellel/src/MyFirstClass.java
@@ -0,0 +1,15 @@
+/**
+ * Created by ES on 17.09.2015.
+ */
+public class MyFirstClass
+{
+    public static void main(String[] args)
+    {
+        String helloMessedge="Hello world";
+        System.out.print(helloMessedge);
+        int ags=23;
+        int myyearbirthday = 1991;
+        System.out.println("I'm " + ags);
+
+}
+}
