0b4d4741f8381cc3cc398502749d455a7a73efa6
Евгений <shah1991@ukr.net>
2015-10-07 22:43:16 +0300

    homewjrk oop

---
+++ hellel/src/homework4/Encryption.java
@@ -0,0 +1,16 @@
+package homework4;
+
+import java.util.Scanner;
+
+/**
+ * Created by Евгений on 07.10.2015.
+ */
+public class Encryption {
+    System.out.println("Enter a key for encryption");
+    String key = enter.nextLine();
+    System.out.println("Enter string for encryption");
+    Scanner enter = new Scanner(System.in);
+    String string = enter.nextLine();
+
+
+}
