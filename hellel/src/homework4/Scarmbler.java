3e59545cdbd418e3975598d863a89ee8a5e97886
Евгений <shah1991@ukr.net>
2015-10-08 15:58:24 +0300

    qwerty

--- hellel/src/homework4/Scarmbler.java
+++ hellel/src/homework4/Scarmbler.java
@@ -5,21 +5,24 @@ package homework4;
  */
 public class Scarmbler {
     private String key;
-    public Scarmbler(String key){
-        this.key=key;
+
+    public Scarmbler(String key) {
+        this.key = key;
     }
-    public String crypt(String message){
-        while (message.length() > key.length()) {
-            key = key + key;
-        }
+
+    public String crypt(String message) {
         char[] array = message.toCharArray();
-        char[] keyarray = key.toCharArray();
+        char[] keyArray = key.toCharArray();
 
         char[] result = new char[array.length];
 
         for (int i = 0; i < array.length; i++) {
-            result[i] = (char) (array[i] ^ keyarray[i]);
+            if (i >= keyArray.length) {
+                result[i] = (char) (array[i] ^ keyArray[i % keyArray.length]);
+            } else {
 
+                result[i] = (char) (array[i] ^ keyArray[i]);
+            }
         }
         String cipher = String.copyValueOf(result);
         return cipher;
