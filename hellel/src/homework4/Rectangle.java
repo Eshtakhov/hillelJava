ba13241200d1f9be6e2f89cdab8c7819c21096a3
Евгений <shah1991@ukr.net>
2015-10-08 15:54:36 +0300

    homewjrk oop

--- hellel/src/homework4/Rectangle.java
+++ hellel/src/homework4/Rectangle.java
@@ -13,6 +13,7 @@ public class Rectangle {
     private double b;
     public Rectangle(double s,double p){
         RectangleTask constr= new RectangleTask(s,p);
+        
         Rectangle rec=constr.getRectangle();
     }
 
@@ -22,8 +23,8 @@ public class Rectangle {
         Scanner enter = new Scanner(System.in);
         double area = enter.nextDouble();
         System.out.println("Enter the perimeter of the rectangle");
-        double perimetre = enter.nextInt();
-        Rectangle example=new Rectangle(area,perimetre);
+        double perimeter = enter.nextInt();
+        Rectangle example=new Rectangle(area,perimeter);
         System.out.printf("The sides of the rectangle are %.2f and %.2f \n", example.a, example.b);
 
     }
