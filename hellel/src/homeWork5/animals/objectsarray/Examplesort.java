362a9046ca34657f789cd79ce32384c85c234ce4
Евгений <shah1991@ukr.net>
2015-10-19 17:45:27 +0300

    homework

--- hellel/src/homeWork5/animals/objectsarray/Examplesort.java
+++ hellel/src/homeWork5/animals/objectsarray/Examplesort.java
@@ -7,17 +7,17 @@ import java.util.Random;
  */
 public class Examplesort {
     public static void main(String[] args) {
-        int[] mas1 = new int[20];
-        int[] mas2 = new int[20];
-        int[] mas3 = new int[20];
+        int [] mas1=new int[20];
+        int [] mas2=new int[20];
+        int [] mas3=new int[20];
         for (int i = 0; i < mas1.length; i++) {
             mas1[i] = (int) (Math.random() * 100);
             System.out.print(mas1[i] + " ");
         }
         System.out.print("\n");
-        Random rand = new Random();
+        Random rand=new Random();
         for (int i = 0; i < mas2.length; i++) {
-            mas2[i] = rand.nextInt();
+            mas2[i] = rand.nextInt(500);
             System.out.print(mas2[i] + " ");
         }
         System.out.print("\n");
@@ -25,22 +25,24 @@ public class Examplesort {
             mas3[i] = (int) (Math.random() * 100);
             System.out.print(mas3[i] + " ");
         }
-        SortingAnArray sortingAnArray = new SortingAnArray();
+        System.out.print("\n");
+        SortingAnArray sortingAnArray=new SortingAnArray();
         sortingAnArray.sortOne(mas1);
         for (int i = 0; i < mas1.length; i++) {
             System.out.print(mas1[i] + " ");
         }
-        System.out.println("\n");
+        System.out.print("\n");
         sortingAnArray.sort2(mas2);
         for (int i = 0; i < mas2.length; i++) {
             System.out.print(mas2[i] + " ");
         }
-        System.out.println("\n");
+        System.out.print("\n");
         sortingAnArray.sort3(mas3);
         for (int i = 0; i < mas3.length; i++) {
             System.out.print(mas3[i] + " ");
         }
-        System.out.println("\n");
+        System.out.print("\n");
+
 
 
     }
