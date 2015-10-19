7e5b61d02885eaa628cb24eab77522a819527204
Евгений <shah1991@ukr.net>
2015-10-19 16:45:50 +0300

    homework

--- hellel/src/homeworkAbstrackInterface/Ellipse.java
+++ hellel/src/homeworkAbstrackInterface/Ellipse.java
@@ -3,16 +3,29 @@ package homeworkAbstrackInterface;
 /**
  * Created by Евгений on 19.10.2015.
  */
-public class Ellipse extends Figure {
+public class Ellipse extends Figure implements PaintingFigure {
     private double radBig;//,большая полуось элипса
     private double radSmall;//ментшаяе полуось элипса
-
+    private Paint paint = new GreenPaint();
 
     public Ellipse(double radiusbig, double radiussmall) {
         radBig = radiusbig;
         radSmall = radiussmall;
 
     }
+
+    @Override
+    public String paintingColor() {
+
+        return paint.getColor();
+
+    }
+
+    @Override
+    public double amountOfInk() {
+        return paint.paintConsumption() * area();
+    }
+
 
     @Override
     public double area() {
