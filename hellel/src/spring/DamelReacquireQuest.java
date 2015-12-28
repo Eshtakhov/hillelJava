08f17df96ff3adbbc561deda50f8d04cb942fa31
ES <shah1991@ukr.net>
2015-12-28 21:05:24 +0200

    IO work class

---
+++ hellel/src/spring/DamelReacquireQuest.java
@@ -0,0 +1,11 @@
+package spring;
+
+/**
+ * Created by ES on 28.12.2015.
+ */
+public class DamelReacquireQuest implements Quest{
+    @Override
+    public void emberk(){
+        System.out.println("Quset start");
+    }
+}
