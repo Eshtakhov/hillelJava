ae3629734d75bc79ef05cf4781e4e93c16966ac1
ES <shah1991@ukr.net>
2016-01-11 20:56:45 +0200

    rtwerqwrqwe

--- hellel/src/spring/SpringMain.java
+++ hellel/src/spring/SpringMain.java
@@ -1,16 +1,29 @@
 package spring;
 
-import javafx.application.Application;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import spring.medival.DamselRescuingKnight;
+import spring.soundSystem.CDPlayer;
 
 /**
- * Created by ES on 28.12.2015.
+ * Created by User on 28.12.2015.
  */
 public class SpringMain {
     public static void main(String[] args) {
-        //ApplicationContext
-        DamperReacquireKnight knight=new DamperReacquireKnight();
-        knight.setQuest(new DamelReacquireQuest());
-        knight.startQuest();
+        ApplicationContext context = new AnnotationConfigApplicationContext(SpringConfig.class);
 
+        DamselRescuingKnight knight = context.getBean(DamselRescuingKnight.class);
+        context.getBean(CDPlayer.class).play();
+        CDPlayer player1=context.getBean(CDPlayer.class);
+        player1.play();
+        CDPlayer player2=context.getBean(CDPlayer.class);
+        player2.play();
+        if(player1==player2){
+            System.out.printf("singleton");
+        }else{
+            System.out.println("prototype");
+        }
+
+        knight.embarkQuest();
     }
 }
